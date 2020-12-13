b1a5d5dba7dfe44c3ad9d7cabb9c02b63768fab9
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-13 17:53:43 +0000

    Removed home app and replaced with webpages app

--- webpages/views.py
+++ webpages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    return render(request, 'index.html')
