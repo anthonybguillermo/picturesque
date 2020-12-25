947507d5e2d1694b041f642e624ab8020d545492
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-25 23:44:24 +0000

    Added posters, views and templates

--- picturesque/urls.py
+++ picturesque/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('webpages.urls')),
+    path('posters/', include('posters.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posters/views.py
+++ posters/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Poster
 
 # Create your views here.
+
+def all_posters(request):
+    """ A view to show all posters, including sorting and search queries """
+
+    posters = Poster.objects.all()
+
+    context = {
+        'posters': posters,
+    }
+
+    return render(request, 'posters/posters.html', context)
