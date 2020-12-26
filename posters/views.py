d0ce8e53f1533ff4d06b9583c997d78a91bd73d5
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-26 01:40:03 +0000

    Added search functionality

--- posters/views.py
+++ posters/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Poster
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_posters(request):
     """ A view to show all posters, including sorting and search queries """
 
     posters = Poster.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria was entered!")
+                return redirect(reverse('posters'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            posters = posters.filter(queries)
 
     context = {
         'posters': posters,
+        'search_term': query,
     }
 
     return render(request, 'posters/posters.html', context)
