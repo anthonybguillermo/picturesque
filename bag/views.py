64b0c9c537d863ee6e04a8ac384e0190c26cd0ae
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-27 12:38:33 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     frame = None
     bag = request.session.get('bag', {})
-    if 'poster_size' in request.POST:
+    if 'poster_frame' in request.POST:
         frame = request.POST['poster_frame']
     bag = request.session.get('bag', {})
 
@@ -34,3 +34,52 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    frame = None
+    bag = request.session.get('bag', {})
+    if 'poster_frame' in request.POST:
+        frame = request.POST['poster_frame']
+    bag = request.session.get('bag', {})
+
+    if frame:
+        if quantity > 0:
+            bag[item_id]['items_by_frame'][frame] = quantity
+        else:
+            del bag[item_id]['items_by_frame'][frame]
+            if not bag[item_id]['items_by_frame']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+    try:
+        frame = None
+        if 'poster_frame' in request.POST:
+            frame = request.POST['poster_frame']
+        bag = request.session.get('bag', {})
+
+        if frame:
+            del bag[item_id]['items_by_frame'][frame]
+            if not bag[item_id]['items_by_frame']:
+                bag.pop(item_id)
+        else:     
+                bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
