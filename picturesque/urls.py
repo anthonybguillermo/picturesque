4df4af4255ee53e6925bf271b33fc77a91afbb11
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-27 21:28:38 +0000

    Added checkout views and templates

--- picturesque/urls.py
+++ picturesque/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('webpages.urls')),
     path('posters/', include('posters.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
