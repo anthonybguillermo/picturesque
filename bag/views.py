508a39cb0844f0de2e96cc10159644ca7b2cc4cf
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-27 17:38:27 +0000

    Added notifications, Css, shopping bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from posters.models import Poster
@@ -13,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    poster = Poster.objects.get(pk=item_id)
+    poster = get_object_or_404(Poster, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     frame = None
@@ -26,13 +26,17 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if frame in bag[item_id]['items_by_frame'].keys():
                 bag[item_id]['items_by_frame'][frame] += quantity
+                messages.success(request, f'Updated frame {frame.upper()} {frame.name} quantity to {bag[item_id]["items_by_frame"][frame]}')
             else:
                 bag[item_id]['items_by_frame'][frame] = quantity
+                messages.success(request, f'Added frame {poster.upper()} {poster.name} to your bag')
         else:
             bag[item_id] = {'items_by_frame': {frame: quantity}}
+            messages.success(request, f'Added frame {poster.upper()} {poster.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated {poster.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {poster.name} to your bag')
@@ -44,6 +48,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the specified product to the specified amount """
 
+    poster = get_object_or_404(Poster, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     frame = None
     bag = request.session.get('bag', {})
@@ -54,15 +59,19 @@ def adjust_bag(request, item_id):
     if frame:
         if quantity > 0:
             bag[item_id]['items_by_frame'][frame] = quantity
+            messages.success(request, f'Updated frame {frame.upper()} {frame.name} quantity to {bag[item_id]["items_by_frame"][frame]}')
         else:
             del bag[item_id]['items_by_frame'][frame]
             if not bag[item_id]['items_by_frame']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed frame {poster.upper()} {poster.name} from your bag') 
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {poster.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {poster.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -70,7 +79,9 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """ Remove the item from the shopping bag """
+
     try:
+        poster = get_object_or_404(Poster, pk=item_id)
         frame = None
         if 'poster_frame' in request.POST:
             frame = request.POST['poster_frame']
@@ -80,11 +91,14 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_frame'][frame]
             if not bag[item_id]['items_by_frame']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed frame {poster.upper()} {poster.name} from your bag')
         else:     
-                bag.pop(item_id)
+            bag.pop(item_id)
+            messages.success(request, f'Removed {poster.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
