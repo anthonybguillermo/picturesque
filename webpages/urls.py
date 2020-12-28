8806d59d0c821adcfbfb46b08f07b59b8cfcf6a9
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-28 02:21:41 +0000

    Fixed home url, nav links, added faqs, contact us and out story pages

--- webpages/urls.py
+++ webpages/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import our_story, contact_us, faqs
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('our_story/', our_story, name="our_story"),
+    path('contact_us/', contact_us, name="contact_us"),
+    path('faqs/', faqs, name="faqs"),
 ]
--- webpages/views.py
+++ webpages/views.py
@@ -5,4 +5,19 @@ from django.shortcuts import render
 def home(request):
     """ A view to return the index page """
 
-    return render(request, 'index.html')
+    return render(request, 'webpages/index.html')
+
+def our_story(request):
+    """ A view to return the our story page """
+
+    return render(request, 'webpages/our_story.html')
+
+def faqs(request):
+    """ A view to return the our faqs page """
+
+    return render(request, 'webpages/faqs.html')
+
+def contact_us(request):
+    """ A view to return the our contact us page """
+
+    return render(request, 'webpages/contact_us.html')
