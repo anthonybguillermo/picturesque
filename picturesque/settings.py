470f7898cb06efcc755ae6c274459b995a3837b8
Anthony Guillermo <a.guillermo8@yahoo.com>
2020-12-30 17:31:36 +0000

    Added secret key for heroku and changed debug to development

--- picturesque/settings.py
+++ picturesque/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!^ubl$@i4nflic3ky$*!t1eb339q43636sf9b_7kvu^e%5hfkz'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['abg-picturesque-2.herokuapp.com', 'localhost']
 
